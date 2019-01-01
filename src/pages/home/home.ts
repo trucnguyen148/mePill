6c19d1125ea0d98bb14c1e39fe08208dc51a7cb0
Nguyen Duy Anh <duyanh19962012@gmail.com>
2019-01-01 19:18:40 +0200

    Change display of Homepage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Slides, Toggle } from 'ionic-angular';
 
 /**
  * Generated class for the HomePage page.
@@ -16,30 +16,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class HomePage {
   slides = [
       {
-        time: "8:00",
+        time: "8:00 Sáng",
         description: "Đã uống",
         image: "assets/imgs/pill.png",
-        name: "PENICILIN...100MG",
+        name: "PENICILIN ..... 100MG",
         number: "Số lượng 1 viên",
-        name1: "PENICILIN...100MG",
+        name1: "PENICILIN ..... 100MG",
         number1: "Số lượng 1 viên",
-        name2: "PENICILIN...100MG",
+        name2: "PENICILIN ..... 100MG",
         number2: "Số lượng 1 viên",
-        name3: "PENICILIN...100MG",
+        name3: "PENICILIN ..... 100MG",
         number3: "Số lượng 1 viên",
-
+        name4: "PENICILIN  ..... 100MG",
+        number4: "Số lượng 1 viên",
+        name5: "PENICILIN ..... 100MG",
+        number5: "Số lượng 1 viên",
+        name6: "PENICILIN ..... 100MG",
+        number6: "Số lượng 1 viên",
+        color: "#0c63f0"
       },
 
       {
-        time: "12:00",
+        time: "12:00 Trưa",
         description: "Chưa uống",
         image: "assets/imgs/pill.png",
+        color: "#FC231C"
       },
 
       {
-        time: "19:00",
+        time: "19:00 Tối",
         description: "Chưa uống",
         image: "assets/imgs/pill.png",
+        color: "#FC231C"
       }
     ];
 
